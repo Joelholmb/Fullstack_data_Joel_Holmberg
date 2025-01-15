a2d745e577af33affcbc51bea7abc9b83886b223
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2025-01-15 13:40:18 +0100

    codealong 12

---
+++ code_alongs/12_packaging_python_project/utils/run_dashboard.py
@@ -0,0 +1,11 @@
+from subprocess import run
+from utils.constants import FRONTEND_PATH
+
+
+
+def run_dashboard():
+    run(["streamlit", "run", FRONTEND_PATH / "dashboard.py"])
+
+
+if __name__ == "__main__":
+    run_dashboard()
